c8e6d0e74c8e6a1d804dd95eb9acfa64883883d8
vagechirkov <vagechirkov@gmail.com>
2023-01-07 16:38:05 +0100

    install nano id and add home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,26 @@
-export default function Learning() {
+import Link from 'next/link';
+import {nanoid} from 'nanoid'
+
+export default function Home() {
+    const userId = nanoid();
 
     return (
 
-        <></>
+        <>
+            <Link href={`/${userId}/learning`}>
+                <button>
+                    Learning
+                </button>
+            </Link>
+
+            <Link href={`/${userId}/workshop`}>
+                <button>
+                    Workshop
+                </button>
+            </Link>
+
+
+        </>
     );
 
 
