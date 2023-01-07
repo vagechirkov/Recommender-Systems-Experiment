75e02407d0784930f11a9bb4404ae6251e1937b1
vagechirkov <vagechirkov@gmail.com>
2023-01-07 16:09:46 +0100

    update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+
+    output: 'standalone',
 }
 
 module.exports = nextConfig
