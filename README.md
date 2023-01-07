f83bc96771ec4870cb56fef4ecca4c88c12c1bd9
vagechirkov <vagechirkov@gmail.com>
2023-01-07 18:01:54 +0100

    install Flowbite

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Recommender-Systems-Experiment
+
+
+### Frontend
+
+#### [Flowbite](https://flowbite.com/docs/getting-started/next-js/)
+
+```zsh
+
+npm install flowbite flowbite-react --save
+
+
+```
--- frontend/app/[playerId]/workshop/page.tsx
+++ frontend/app/[playerId]/workshop/page.tsx
@@ -11,7 +11,7 @@ export default function Workshop({params,}: {
                 </button>
             </Link>
 
-            <div className="grid grid-rows-3 grid-flow-col gap-4">
+            <div className="grid justify-around grid-rows-3 grid-flow-col gap-4">
                 <div className="row-span-3 bg-sky-500">01</div>
                 <div className="col-span-2">02</div>
                 <div className="row-span-2 col-span-2">03</div>
